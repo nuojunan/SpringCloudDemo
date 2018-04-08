e795eb7d1b7f72fac2e78eda1524fd752255bedc
lindeyi <lindeyi@bjnja.com>
2018-04-08 11:23:24 +0800

    spring boot多语言处理

---
+++ i18n-demo/src/main/java/com/bjnja/i18n/demo/I18nController.java
@@ -0,0 +1,42 @@
+package com.bjnja.i18n.demo;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+@RestController
+@RequestMapping("i18n")
+public class I18nController {
+
+	@Autowired
+	private MessageSource messageSource;
+	
+	@GetMapping("/hi")
+	public String hi() {
+		Locale locale = LocaleContextHolder.getLocale();
+		String message = messageSource.getMessage("hi", null, locale);
+		System.out.println("hi=="+locale.getLanguage()+"==" + message);
+		return "say: hi" + message;
+	}
+	
+	@GetMapping("/set")
+	public String set(HttpServletRequest request, HttpServletResponse response, String lang) {
+	   LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+       if("zh".equals(lang)){
+           localeResolver.setLocale(request, response, new Locale("zh"));
+       }else if("en".equals(lang)){
+           localeResolver.setLocale(request, response, new Locale("en"));
+       }
+       return lang;
+	}
+}
---
+++ i18n-demo/src/main/java/com/bjnja/i18n/demo/LocaleConfig.java
@@ -0,0 +1,40 @@
+package com.bjnja.i18n.demo;
+
+import java.util.Locale;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class LocaleConfig extends WebMvcConfigurerAdapter{
+
+	@Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        // 默认语言
+        slr.setDefaultLocale(Locale.ENGLISH);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        // 设置请求地址的参数,默认为：locale
+//        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
